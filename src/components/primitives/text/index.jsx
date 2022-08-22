fb47494acb31ebfe1694c0d1ea4ef33de35e7ba1
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-22 18:38:41 +0300

    add text component

---
+++ src/components/primitives/text/index.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+function Text({ children, color, size }) {
+  return <span style={{ color, fontSize: size }} className="text">{children}</span>
+}
+
+export default Text
+Text.propTypes = {
+  /**
+   * Content to put inside block. Can be string or other react node (Icon e.g.)
+   */
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  /**
+   * Text color value
+   */
+  color: PropTypes.string,
+  /**
+   * Text size value
+   */
+  size: PropTypes.string,
+}
+Text.defaultProps = {
+  color: "#5B5B5B",
+  size: "16px",
+}
---
+++ src/components/primitives/text/text.stories.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react"
+import Text from "./index"
+import Icon from "../icon";
+
+export default {
+  title: "primitives/Text",
+  component: Text,
+}
+
+export function DefaultText(args) {
+  return (
+    <Text {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Architecto blanditiis, illo, ipsam magnam nemo, nobis obcaecati
+      officiis ratione similique sint soluta suscipit? Corporis culpa
+      fuga iure, minus perferendis possimus quibusdam quisquam reiciendis
+      sed soluta, voluptate voluptatibus! Autem impedit incidunt maxime
+      minima perspiciatis placeat quaerat saepe totam ullam,
+      vel voluptate voluptatem!
+    </Text>
+  )
+}
+DefaultText.args = {
+  color: "#5B5B5B",
+  size: "16px"
+}
+
+export function WithNodesInside(args) {
+  return (
+    <Text {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      <Icon color="#FFA959" size="18px" name="CustomDiscord" />
+      <Icon color="#FFA959" size="18px" name="CustomTelegram" />
+      <Icon color="#FFA959" size="18px" name="CustomTwitter" />
+    </Text>
+  )
+}
+WithNodesInside.args = {
+  color: "#5B5B5B",
+  size: "16px"
+}
