87a2d9e809e3881044f262e671f1c92ecd02a62e
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-23 20:47:08 +0300

    primitives index file

---
+++ src/components/primitives/index.js
@@ -0,0 +1,9 @@
+import AdaptiveLink from "./adaptiveLink"
+import Button from "./button"
+import Heading from "./heading"
+import Icon from "./icon"
+import Image from "./image"
+import InputText from "./inputText"
+import Text from "./text"
+
+export { AdaptiveLink, Button, Heading, Icon, Image, InputText, Text }
