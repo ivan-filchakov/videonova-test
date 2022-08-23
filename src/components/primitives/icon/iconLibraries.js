4581fd48f62a0b1815bd18b81d4f060fbb051ad4
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-23 03:05:06 +0300

    add new icon

--- src/components/primitives/icon/iconLibraries.js
+++ src/components/primitives/icon/iconLibraries.js
@@ -2,31 +2,33 @@ import * as AntIcons from "react-icons/ai"
 import * as BootstrapIcons from "react-icons/bs"
 import * as FontAwesome from "react-icons/fa"
 import * as MaterialDesign from "react-icons/md"
-import { ReactComponent as Check } from "./svg/check.svg"
-import { ReactComponent as Cross } from "./svg/cross.svg"
-import { ReactComponent as Discord } from "./svg/discord.svg"
-import { ReactComponent as Eye } from "./svg/eye.svg"
-import { ReactComponent as Heart } from "./svg/heart.svg"
-import { ReactComponent as Rating } from "./svg/rating.svg"
-import { ReactComponent as Settings } from "./svg/settings.svg"
-import { ReactComponent as Telegram } from "./svg/telegram.svg"
-import { ReactComponent as Twitter } from "./svg/twitter.svg"
-import { ReactComponent as VideoFill } from "./svg/video-fill.svg"
-import { ReactComponent as VideoOutline } from "./svg/video-outline.svg"
+import { ReactComponent as CustomCheck } from "./svg/check.svg"
+import { ReactComponent as CustomCross } from "./svg/cross.svg"
+import { ReactComponent as CustomDiscord } from "./svg/discord.svg"
+import { ReactComponent as CustomEye } from "./svg/eye.svg"
+import { ReactComponent as CustomEyeClosed } from "./svg/eye-closed.svg"
+import { ReactComponent as CustomHeart } from "./svg/heart.svg"
+import { ReactComponent as CustomRating } from "./svg/rating.svg"
+import { ReactComponent as CustomSettings } from "./svg/settings.svg"
+import { ReactComponent as CustomTelegram } from "./svg/telegram.svg"
+import { ReactComponent as CustomTwitter } from "./svg/twitter.svg"
+import { ReactComponent as CustomVideoFill } from "./svg/video-fill.svg"
+import { ReactComponent as CustomVideoOutline } from "./svg/video-outline.svg"
 import { ReactComponent as LogoMain } from "./svg/logo-main.svg"
 
 const CustomIcons = {
-  CustomCheck: Check,
-  CustomCross: Cross,
-  CustomDiscord: Discord,
-  CustomEye: Eye,
-  CustomHeart: Heart,
-  CustomRating: Rating,
-  CustomSettings: Settings,
-  CustomTelegram: Telegram,
-  CustomTwitter: Twitter,
-  CustomVideoFill: VideoFill,
-  CustomVideoOutline: VideoOutline,
+  CustomCheck,
+  CustomCross,
+  CustomDiscord,
+  CustomEye,
+  CustomEyeClosed,
+  CustomHeart,
+  CustomRating,
+  CustomSettings,
+  CustomTelegram,
+  CustomTwitter,
+  CustomVideoFill,
+  CustomVideoOutline,
   LogoMain,
 }
 
