52f95c85c497529748deb4fc760b50859d0a47b5
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-26 09:53:59 +0300

    add middleware

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,9 @@
 import React from "react"
-import { configureStore, createSlice } from "@reduxjs/toolkit"
+import {
+  configureStore,
+  createListenerMiddleware,
+  createSlice,
+} from "@reduxjs/toolkit"
 import { Provider } from "react-redux"
 import PropTypes from "prop-types"
 import modalReducers from "./reducers/modalReducers"
@@ -32,8 +36,13 @@ const siteInfoSlice = createSlice({
         },
       },
       warningMessage: [
-        "Your Password is incorrect. Please, try again",
+        "Unknown error",
+        "Incorrect login - no 2 spaces in a row",
+        "Incorrect symbols used in login",
+        "No spaces in the password",
+        "Incorrect symbols used in password",
         "Passwords doesn't match! Try again",
+        "Your Password is incorrect. Please, try again",
       ],
       inputLabels: {
         login: "Name",
@@ -45,15 +54,6 @@ const siteInfoSlice = createSlice({
   name: "siteInfo",
 })
 
-const userInfoSlice = createSlice({
-  initialState: {
-    authorized: false,
-    name: "Store Username",
-  },
-  name: "userInfo",
-  reducers: userReducers(),
-})
-
 const modalSlice = createSlice({
   initialState: {
     isOpen: false,
@@ -63,13 +63,34 @@ const modalSlice = createSlice({
   reducers: modalReducers(),
 })
 
+const userSlice = createSlice({
+  initialState: {
+    authorized: false,
+    authError: null,
+    info: null,
+  },
+  name: "user",
+  reducers: userReducers(),
+})
+
+const listenerMiddleware = createListenerMiddleware()
+listenerMiddleware.startListening({
+  actionCreator: userSlice.actions.authorize,
+  effect: (action) => {
+    console.log(action.type)
+  },
+})
+
 export const store = configureStore({
   reducer: {
     siteInfo: siteInfoSlice.reducer,
-    userInfo: userInfoSlice.reducer,
+    user: userSlice.reducer,
     modal: modalSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware),
 })
+export const { authorize } = userSlice.actions
 
 export function Store(props) {
   const { children } = props
--- src/store/reducers/userReducers.js
+++ src/store/reducers/userReducers.js
@@ -2,10 +2,25 @@
 export default function userReducers() {
   return {
     authorize(state) {
+      state.requesting = true
+      // state.authorized = true
+    },
+    authSucces(state, action) {
+      state.requesting = false
       state.authorized = true
+      state.info = action.payload.info
+      state.authError = null
+    },
+    authError(state, action) {
+      state.requesting = false
+      state.authorized = false
+      state.userInfo = null
+      state.authError = action.payload.error.toString()
     },
     unauthorize(state) {
+      state.requesting = false
       state.authorized = false
+      state.authError = null
     },
   }
 }
