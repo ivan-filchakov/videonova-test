df17a91885996e34d4c1469e3955b5e491910aa4
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-26 05:34:14 +0300

    home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,27 @@
 import React from "react"
 import Layout from "../../layout"
+import { Button, Heading, Text } from "../../components/primitives"
+import "./style.css"
 
 function HomePage() {
-  const headingH1 = {
-    fontFamily: "Chillax, sans-serif",
-    fontWeight: 600,
-    fontSize: "24px",
-  }
-
   return (
     <Layout>
-      <div style={headingH1}>VideoNova home page</div>
+      <div className="homeBanner">
+        <div className="homeBanner__heading">
+          <Heading h={1} accent={3} accentColor="#FF6363">
+            Welcome to VideoNova
+          </Heading>
+        </div>
+        <div className="homeBanner__text">
+          <Text>
+            Creative videos with a single click. Add subtitles, transcribe and
+            more
+          </Text>
+        </div>
+        <div className="homeBanner__button">
+          <Button label="Button" variant="main" />
+        </div>
+      </div>
     </Layout>
   )
 }
