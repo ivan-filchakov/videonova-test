1e87caeefd8a8e6c52d69335ec2311cd1e2ca34d
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-29 07:55:42 +0300

    userPage draft

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -2,15 +2,24 @@ import React from "react"
 import { useParams } from "react-router-dom"
 import { useSelector } from "react-redux"
 import Layout from "../../layout"
+import { Heading, Image } from "../../components/primitives"
+import "./style.css"
 
 function UserPage() {
-  const siteInfo = useSelector((store) => store.siteInfo)
-  const user = useParams()
+  const userInfo = useSelector(({ allUsers }) => allUsers.info)
+  const params = useParams()
+
+  const user = userInfo.find((el) => el.slug === params.id)
+  console.log(user)
   return (
     <Layout>
-      <div>
-        page content for user {user.id};<br />
-        message from store: {siteInfo.test}
+      <div className="userPage">
+        <div className="userPage__pic">
+          <Image src={user.userPic} alt={user.userName} fit="cover" />
+        </div>
+        <div className="userPage__heading">
+          <Heading h={1}>{user.userName}</Heading>
+        </div>
       </div>
     </Layout>
   )
