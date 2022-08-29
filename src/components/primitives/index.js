4cf4f108616ed70cb43777a8034eb9cbb2684c49
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-08-29 07:12:48 +0300

    add loader

--- src/components/primitives/index.js
+++ src/components/primitives/index.js
@@ -4,6 +4,7 @@ import Heading from "./heading"
 import Icon from "./icon"
 import Image from "./image"
 import InputText from "./inputText"
+import Loader from "./loader"
 import Text from "./text"
 
-export { AdaptiveLink, Button, Heading, Icon, Image, InputText, Text }
+export { AdaptiveLink, Button, Heading, Icon, Image, InputText, Loader, Text }
