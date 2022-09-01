e91063945fd8804d5f424a49b02659df430f702f
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-01 14:08:46 +0300

    userPage draft 2

--- src/pages/userPage/index.jsx
+++ src/pages/userPage/index.jsx
@@ -1,29 +1,55 @@
-import React from "react"
+/* eslint-disable */
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import Layout from "../../layout"
 import { Heading, Image } from "../../components/primitives"
 import VideosList from "../../components/partials/videosList"
 import "./style.css"
 
 function UserPage() {
-  const params = useParams()
-  const user = useSelector((store) => {
-    const allUsers = store.allUsers.info
-    return allUsers.find((el) => el.slug === params.id)
-  })
-  const pageInfo = useSelector((store) => {
-    console.log(store)
-    return store.siteInfo.userPageInfo
-  })
+  const routeParams = useParams()
+  const dispatch = useDispatch()
+
+  const pageInfo = useSelector((store) => store.siteInfo.userPageInfo)
+  const users = useSelector((store) => store.allUsers.info)
+  const authUser = useSelector(({ user }) => user)
+
+  const [user, setUser] = useState(false)
+  const [admin, setAdmin] = useState(false)
+  useEffect(() => {
+    console.log(1)
+    if (!users) dispatch({ type: "allUsers/request" })
+    console.log(2)
+    if (users) setUser(users.find((el) => el.slug === routeParams.id))
+    console.log(3)
+    if (authUser.authorized && authUser.info.id === user.id) {
+      setAdmin(true)
+    }
+  }, [user, authUser.authorized])
+
+  const loadingBlock = user ? "" : " userPage_gradientBlock"
+  const loadingLine = user ? "" : " gradientLine"
+
+  const openModalUpload = () => {
+    dispatch({
+      type: "modal/placeContent",
+      payload: "FormVideoUpload",
+    })
+  }
+
   return (
     <Layout>
       <div className="userPage">
-        <div className="userPage__pic">
-          <Image src={user.userPic} alt={user.userName} fit="cover" />
+        <div className={`userPage__pic${loadingBlock}`}>
+          {user && (
+            <Image src={user.userPic} alt={user.userName} fit="cover" />
+          )}
         </div>
-        <div className="userPage__heading">
-          <Heading h={1}>{user.userName}</Heading>
+        <div className={`userPage__heading${loadingLine}`}>
+          <Heading h={1} loading={!user}>
+            {user && user.userName}
+          </Heading>
         </div>
         <div className="userPage__content">
           <VideosList
@@ -31,6 +57,9 @@ function UserPage() {
             userName={user.userName}
             heading={pageInfo.videosList.heading}
             buttonLabel={pageInfo.videosList.buttonLabel}
+            isEmpty={pageInfo.videosList.isEmpty}
+            admin={admin}
+            openModalUpload={openModalUpload}
           />
         </div>
       </div>
