bec6894a49cfb082aa528844a609c5b856d19cf2
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-01 06:15:15 +0300

    videoCard component

---
+++ src/components/partials/videoCard/videoCard.stories.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react"
+import VideoCard from "./index"
+
+export default {
+  title: "partials/VideoCard",
+  component: VideoCard,
+  argTypes: {
+    // type: { control: "radio", options: ["text", "password", "textarea"] },
+  },
+}
+
+function Template(args) {
+  return (
+    <div style={{
+      maxWidth: "360px",
+      padding: "20px",
+      border: "1px dashed #ccc",
+    }}>
+      <VideoCard {...args} />
+    </div>
+  )
+}
+export const Default = Template.bind({})
+Default.args = {
+  // proceeds all variations of youtube links:
+  // src: "https://youtu.be/jfKfPfyJRdk",
+  // src: "https://youtube.com/embed/jfKfPfyJRdk",
+  src: "https://www.youtube.com/watch?v=Y3PtV9H9eK4&ab_channel=TopMovieClips",
+  heading: "video heading",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et!",
+}
