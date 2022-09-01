23af3c0c20c7f658bc5ce96b7f1c7c5577379b60
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-01 06:15:08 +0300

    add video player icon

--- src/components/primitives/icon/iconLibraries.js
+++ src/components/primitives/icon/iconLibraries.js
@@ -7,6 +7,8 @@ import { ReactComponent as CustomCross } from "./svg/cross.svg"
 import { ReactComponent as CustomDiscord } from "./svg/discord.svg"
 import { ReactComponent as CustomEye } from "./svg/eye.svg"
 import { ReactComponent as CustomEyeClosed } from "./svg/eye-closed.svg"
+import { ReactComponent as CustomLogo } from "./svg/logo-main.svg"
+import { ReactComponent as CustomPlayVideo } from "./svg/play-video.svg"
 import { ReactComponent as CustomHeart } from "./svg/heart.svg"
 import { ReactComponent as CustomRating } from "./svg/rating.svg"
 import { ReactComponent as CustomSettings } from "./svg/settings.svg"
@@ -14,7 +16,6 @@ import { ReactComponent as CustomTelegram } from "./svg/telegram.svg"
 import { ReactComponent as CustomTwitter } from "./svg/twitter.svg"
 import { ReactComponent as CustomVideoFill } from "./svg/video-fill.svg"
 import { ReactComponent as CustomVideoOutline } from "./svg/video-outline.svg"
-import { ReactComponent as CustomLogo } from "./svg/logo-main.svg"
 
 const CustomIcons = {
   CustomCheck,
@@ -23,13 +24,14 @@ const CustomIcons = {
   CustomEye,
   CustomEyeClosed,
   CustomHeart,
+  CustomLogo,
+  CustomPlayVideo,
   CustomRating,
   CustomSettings,
   CustomTelegram,
   CustomTwitter,
   CustomVideoFill,
   CustomVideoOutline,
-  CustomLogo,
 }
 
 export default function useIconLibraries() {
