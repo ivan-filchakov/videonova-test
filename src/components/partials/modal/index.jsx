53d6d146c520cd00a8af65816a47e8b29a5dbecd
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-07 01:21:41 +0300

    upd media styles for mobile layout

--- src/components/partials/modal/index.jsx
+++ src/components/partials/modal/index.jsx
@@ -1,18 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "./style.css"
 import { AdaptiveLink, Icon } from "../../primitives"
+import "./style.css"
+import "./style.media.css"
 
 function Modal({ children, isOpen, onClose }) {
   if (isOpen)
     return (
       <div className="modal">
         <div className="modal__content">
-          {children}
           <div className="modal__close">
             <AdaptiveLink action={onClose} stretch />
             <Icon color="#000" size="16px" name="CustomCross" />
           </div>
+          {children}
         </div>
         <div className="modal__bg">
           <AdaptiveLink action={onClose} stretch />
